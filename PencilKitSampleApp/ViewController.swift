d46f936f280a9f2226de68ff7bdf3531a13f4bee
Dinesh <34174856+dinesh2910@users.noreply.github.com>
2020-06-14 20:46:36 -0500

    PKToolPicker Setup

--- PencilKitSampleApp/ViewController.swift
+++ PencilKitSampleApp/ViewController.swift
@@ -9,16 +9,33 @@
 import UIKit
 import PencilKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
 
     
     @IBOutlet weak var pencilFingerToggleBtn: UIBarButtonItem!
     @IBOutlet weak var canvesView: PKCanvasView!
     
+    let canvesWidth: CGFloat = 768
+    let canvesScrollHeight: CGFloat = 500
+    
+    var drawing = PKDrawing()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        canvesView.delegate = self
+        canvesView.drawing = drawing
+        canvesView.alwaysBounceVertical = true
+        canvesView.allowsFingerDrawing = true
+        
+        if let window = parent?.view.window,
+            let toolPicker = PKToolPicker.shared(for: window) {
+            toolPicker.setVisible(true, forFirstResponder: canvesView)
+            toolPicker.addObserver(canvesView)
+            canvesView.becomeFirstResponder()
+        }
     }
+    
 
     @IBAction func didPressSaveToAlbumButton(_ sender: UIBarButtonItem) {
         
